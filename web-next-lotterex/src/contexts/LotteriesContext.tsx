08ef6363295a92ca6020d58274125de63eeb7139
zS1L3NT Mac <zechariahtan144@gmail.com>
2023-05-31 16:21:10 +0800

    ✨ error when lottery id is invalid

--- web-next-lotterex/src/contexts/LotteriesContext.tsx
+++ web-next-lotterex/src/contexts/LotteriesContext.tsx
@@ -1,11 +1,15 @@
 import { createContext, PropsWithChildren, useContext, useEffect, useState } from "react"
-import { Contract } from "web3"
+import { AppContract } from "web3-eth-contract"
+
+import { Code } from "@mantine/core"
+import { notifications } from "@mantine/notifications"
+import { IconX } from "@tabler/icons-react"
 
 import LotterexArtifact from "../contracts/Lotterex.json"
 import WalletContext from "./WalletContext"
 
 const LotteriesContext = createContext({
-	lotteries: [] as Contract[],
+	lotteries: [] as AppContract[],
 	addLotteryId: (lotteryId: string) => {}
 })
 
@@ -14,12 +18,31 @@ export const LotteriesProvider = ({ children }: PropsWithChildren) => {
 	const artifact = LotterexArtifact as any
 	const { web3 } = useContext(WalletContext)
 
-	const [lotteries, setLotteries] = useState<Contract[]>([])
+	const [lotteries, setLotteries] = useState<AppContract[]>([])
 
 	useEffect(() => {
 		if (web3) {
 			const lotteryIds = JSON.parse(localStorage.getItem("lotteries") || "[]") as string[]
-			setLotteries(lotteryIds.map(l => new web3.eth.Contract(artifact.abi, l)))
+			const lotteries = []
+
+			for (const lotteryId of lotteryIds) {
+				try {
+					const lottery = new web3.eth.Contract(artifact.abi, lotteryId)
+					lotteries.push(lottery)
+				} catch {
+					notifications.show({
+						withCloseButton: true,
+						autoClose: false,
+						title: "Invalid Lottery ID",
+						message: <Code>{lotteryId}</Code>,
+						color: "red",
+						icon: <IconX />
+					})
+				}
+			}
+
+			localStorage.setItem("lotteries", JSON.stringify(lotteries.map(l => l.options.address)))
+			setLotteries(lotteries)
 		}
 	}, [web3])
 
